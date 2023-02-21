ce2e3fec61682bae89f922783e51bd5df4ee045b
isaiahbob1 <isaiahbobodotun@gmail.com>
2023-02-21 04:34:16 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include "softwareengineer.h"
+
+/**
+ * main - print softwareengineer
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+_putchar('s');
+_putchar('o');
+_putchar('f');
+_putchar('t');
+_putchar('w');
+_putchar('a');
+_putchar('r');
+_putchar('e');
+_putchar('e');
+_putchar('n');
+_putchar('g');
+_putchar('i');
+_putchar('n');
+_putchar('e');
+_putchar('e');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
+
+
+
